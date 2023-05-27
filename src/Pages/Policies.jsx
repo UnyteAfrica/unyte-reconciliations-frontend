cb5ed1f18885fbb496f1d2f0c89e3502d8bc2392
Gabriel Shoyombo <gabrielshoyombo2002@gmail.com>
2023-05-27 14:44:49 +0100

    worked on the policies, 60% complete, worked on searchbar component, 75% complete

--- src/Pages/Policies.jsx
+++ src/Pages/Policies.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import FilterBtn from "../Components/FilterBtn";
 import DownloadBtn from "../Components/DownloadBtn";
+import Pagination from "../Components/Pagination";
 
 import Table from "../Components/Table";
+import SearchBar from "../Components/SearchBar";
+//import { paginate } from "../utils/paginate";
 
 function Policies() {
   const tableHead = [
@@ -13,6 +16,18 @@ function Policies() {
     "Price",
   ];
   const [tableBody, setTableBody] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handleSearch = () => {
+    //
+  }
+
+  const pageSize = 10;
+  const count = tableBody.length;
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  }
 
   useEffect(() => {
     setTableBody([
@@ -60,13 +75,17 @@ function Policies() {
           <span className="text-3xl text-[#333333] font-semibold">
             Policies
           </span>
+          <SearchBar handleSearch={handleSearch} placeholder="Find policy reference" />
           <div id="dates" className="flex flex-row items-center space-x-3">
             <FilterBtn />
             <DownloadBtn />
           </div>
         </div>
-        <div id="policies-table">
+        <div id="policies-table" className="mb-10">
           <Table tableHead={tableHead} tableBody={tableBody} />
+        </div>
+        <div className="mb-40">
+            <Pagination itemsCount={count} pageSize={pageSize} onPageChange={handlePageChange} currentPage={currentPage}/>
         </div>
       </div>
     </>
