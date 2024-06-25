1be846ff285950d4b2c609a40c3cf3bb599117d9
Seun Taiwo <lorddro1532@gmail.com>
2024-06-25 12:30:19 +0100

    SCRUM-141 corrected forget password text

--- src/pages/agent/forgot-password.tsx
+++ src/pages/agent/forgot-password.tsx
@@ -34,7 +34,7 @@ export const AgentForgotPasswordPage = () => {
         >
           <header>
             <p className="text-center mb-11 font-medium text-[28px]">
-              Agent Forget Password
+              Forgot Password
             </p>
           </header>
           <div className="space-y-6">
